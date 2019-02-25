7cec3b770233146d1b61c69561db198f920670b2
Kenneth Arvin Rodriguez <krodriguez@cambridge.org>
2019-02-25 21:31:21 +0800

    Adds shipping and billing address selection.

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.shipping-address-list .card-body').click(function () {
+        $('.shipping-address-list .card-body').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    $('#billToSameAddress').change(function () {
+        $('.billing-address-list').toggleClass('d-none');
+    });
+});
